24ddb3b39fd172c8db63dcb1a3ba89a8d4e52a0d
ZhabskyiO <zhabskyio@gmail.com>
2025-05-21 12:02:08 -0600

    updates configs for vite and publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true
+    })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ZhabskyiComponentsMui",
+      formats: ["es", "umd"],
+      fileName: (format) => `zhabskyi-components-mui.${format}.js`
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "@mui/material", "@emotion/react", "@emotion/styled"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "@mui/material": "MaterialUI",
+          "@emotion/react": "emotionReact",
+          "@emotion/styled": "emotionStyled"
+        }
+      }
+    }
+  }
+});
