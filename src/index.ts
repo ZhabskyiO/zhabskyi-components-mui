2a8869e0918a86f57a725cd8fa2aa14242cf0d5e
ZhabskyiO <zhabskyio@gmail.com>
2025-05-21 16:47:38 -0600

    bump up version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { default as Button } from "./components/Button";
 export { default as LoadingIndicator } from "./components/LoadingIndicator";
-export { themes, type Theme } from "./theme/theme";
+export { themes } from "./theme/theme";
+
+export type { Theme } from "./theme/theme";
 export { ThemeProvider } from "@mui/material/styles";
