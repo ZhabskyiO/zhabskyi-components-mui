b2a4ee52128033931e344e2af94e4d812e86cf3e
ZhabskyiO <zhabskyio@gmail.com>
2025-05-21 23:19:07 -0600

    feat:[test] trigger release

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,5 @@ export { themes } from "./theme/theme";
 export type { Themes } from "./theme/theme";
 
 export type { Theme } from "./theme/theme";
+
 export { ThemeProvider } from "@mui/material/styles";
